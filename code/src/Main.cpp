1f727c6255870df8570eb1ee085d29ea9ead57a1
andern <andern@gmail.com>
2013-04-26 20:16:06 +0200

    Added set benchmark.

--- code/src/Main.cpp
+++ code/src/Main.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <algorithm>
 
+#include <string>
+#include <sstream>
+
 #include "coin/ClpModel.hpp"
 #include "coin/ClpInterior.hpp"
 #include "coin/ClpCholeskyBase.hpp"
@@ -14,43 +17,44 @@
 #include <cstdlib>
 #include <ctime>
 
-template <size_t N>
-static std::set<uint16_t>* copy(const std::bitset<N>* a) {
+#define N 65
+#define SIZE 32*N
+
+static std::set<uint16_t>* copy(const std::bitset<SIZE>* a) {
     std::set<uint16_t>* res = new std::set<uint16_t>;
-    for (uint16_t i = 0; i < N; i++) {
+    for (uint16_t i = 0; i < SIZE; i++) {
         if (a->test(i)) res->insert(i);
     }
     return res;
 }
 
 int main() {
-/*
+
     ClpModel model = clpFromTxt();
 
     double t1 = omp_get_wtime();
-//    struct vertex* root = construct(model, 2, 10000, 1e-7);
-    construct_all(model, 2, 10000, 1e-7);
+    struct vertex* root = construct_clp(model, 2, 10000, 1e-7);
+//    construct_all(model, 2, 10000, 1e-7);
     double t2 = omp_get_wtime();
     std::cout.precision(20);
     std::cout << "Solved all cases in " << (t2-t1) << " seconds." << std::endl;
-//    delete root;
-    */
-    srand((unsigned int)time(NULL));
-    std::vector<std::bitset<32>*> tempsets;
-    std::vector<std::bitset<32>*> bitsets;
+    delete root;
+    
+/*    srand((unsigned int)time(NULL));
 
-    int size = 2;
+    std::vector<std::bitset<SIZE>*> bitsets;
 
-    for (int i = 0; i < 1000; i++) {
-        tempsets.push_back(new std::bitset<32>(rand()));
-        for (int j = 0; j < 2; j++) {
+    for (int i = 0; i < 3000; i++) {
+        std::stringstream ss;
+        for (int j = 0; j < N; j++) {
+            std::bitset<32> bs(rand());
+            ss << bs.to_string();
         }
+        bitsets.push_back(new std::bitset<SIZE>(ss.str()));
     }
 
-
-
     std::vector<std::set<uint16_t>*> sets;
-    for (int i = 0; i < 1000; i++) {
+    for (int i = 0; i < 3000; i++) {
         sets.push_back(copy(bitsets[i]));
     }
 
@@ -63,17 +67,18 @@ int main() {
         }
     }
     double t2 = omp_get_wtime();
-    std::cout << fool << std::endl;
+    //std::cout << fool << std::endl;
 
     for (uint16_t i = 0; i < bitsets.size(); i++) {
-        std::bitset<2000> notb = ~(*bitsets[i]);
+        std::bitset<SIZE> notb = ~(*bitsets[i]);
         for (uint16_t j = 0; j < bitsets.size(); j++) {
             fool = isSubset_bit(notb, *bitsets[j]);
         }
     }
     double t3 = omp_get_wtime();
-    std::cout << fool << std::endl;
+    //std::cout << fool << std::endl;
 
-    std::cout << "sets: " << (t2-t1) << " seconds.\n";
-    std::cout << "bitsets: " << (t3-t2) << " seconds.\n";
+    std::cout << "(" << SIZE << ", " << (t2-t1) << ")\n";
+    std::cout << "(" << SIZE << ", " << (t3-t2) << ")\n";
+    */
 }
